4eb96d88769ea39cffd7f43c1d5efbc7babe6490
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-03-24 19:24:28 +0100

    Prueba

--- JavaModC2ArraylistHashtable/src/Ejercicio4App.java
+++ JavaModC2ArraylistHashtable/src/Ejercicio4App.java
@@ -15,6 +15,8 @@ public class Ejercicio4App {
 	/**
 	 * @param args
 	 */
+	
+	//PRUEBA
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejercicio 4 - CONTROL DE VENTAS-STOCK");
